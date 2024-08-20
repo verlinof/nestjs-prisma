5d8e5c237d6939d2e9d7676eb5c21555f4fea03e
verlinof <ajikoko.zx@gmail.com>
2024-08-20 09:49:16 +0700

    Feat: Headers and Redirect

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HeroModule } from './hero/hero.module';
+import { UsersModule } from './users/users.module';
+import { RoleController } from './role/role.controller';
 
 @Module({
-  imports: [HeroModule],
-  controllers: [AppController],
+  imports: [HeroModule, UsersModule],
+  controllers: [AppController, RoleController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/hero/hero.controller.ts
+++ src/hero/hero.controller.ts
@@ -8,11 +8,32 @@ import {
   Delete,
   HttpCode,
   Res,
+  Req,
+  Header,
+  Redirect,
 } from '@nestjs/common';
 import { HeroService } from './hero.service';
 import { CreateHeroDto } from './dto/create-hero.dto';
 import { UpdateHeroDto } from './dto/update-hero.dto';
 import { PassThrough } from 'stream';
+
+const heroes = [
+  {
+    id: 1,
+    name: 'Spiderman',
+    description: 'Superhero',
+  },
+  {
+    id: 2,
+    name: 'Ironman',
+    description: 'Superhero',
+  },
+  {
+    id: 3,
+    name: 'Hulk',
+    description: 'Superhero',
+  },
+];
 
 @Controller('hero')
 export class HeroController {
@@ -21,12 +42,37 @@ export class HeroController {
   @Get()
   @HttpCode(200) //Contoh decorator untuk custom httpcode
   index(@Res() response) {
-    return response.json({ message: 'Hello Hero' }); //Response dengan @Res
+    return response.json({ data: heroes }); //Response dengan @Res
   }
 
   @Post()
-  create(@Body() createHeroDto: CreateHeroDto) {
-    return this.heroService.create(createHeroDto);
+  @Header('Content-Type', 'application/json')
+  store(
+    @Body() createHeroDto: CreateHeroDto,
+    @Req() request,
+    @Res({ passthrough: true }) response,
+  ) {
+    try {
+      // return this.heroService.create(createHeroDto);
+      // return {
+      //   status: 201,
+      //   data: request.body,
+      // }; // Return dengan library Nest
+      const { id, name, description } = request.body;
+      heroes.push({ id, name, description });
+
+      return response
+        .status(201)
+        .json({ message: 'Hero created', data: request.body }); //Response dengan @Res express
+    } catch (error) {
+      return response.status(500).json({ message: 'Internal server error' });
+    }
+  }
+
+  @Get('docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  docs() {
+    return { url: 'https://docs.nestjs.com' };
   }
 
   @Get(':id')
